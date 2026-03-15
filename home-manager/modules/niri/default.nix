a48cc339d764a06cc437f6d217b9821b81fbf0a2
matswuuu <matswuuu@gmail.com>
2026-03-16 01:35:12 +1000

    .

--- home-manager/modules/niri/default.nix
+++ home-manager/modules/niri/default.nix
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:home-manager/modules/niri/default.nix
 { pkgs, ... }:
 
 {
@@ -26,35 +25,3 @@
     };
   };
 }
-=======
-  let
-    vars = import ./../../../vars.nix;
-  in
-{
-  home.file = {
-    ".config/hypr/common.conf".source = ./common.conf;
-    ".config/hypr/binds.conf".source = ./binds.conf;
-    ".config/hypr/env.conf".source = ./env.conf;
-    ".config/hypr/main/monitor.conf".source = ./main/monitor.conf;
-    ".config/hypr/laptop/monitor.conf".source = ./laptop/monitor.conf;
-    ".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
-    ".config/hypr/wallpapers" = {
-      source = ./wallpapers;
-      recursive = true;
-    };
-  };
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland = {
-      enable = true;
-    };
-    settings = {
-      source = [
-        "~/.config/hypr/common.conf"
-        "~/.config/hypr/env.conf"
-        "~/.config/hypr/binds.conf"
-      ] ++ (vars.profiles.${vars.activeProfile}.hyprland or []);
-    };
-  };
-}
->>>>>>> 43f07eae7f18a275dcfbb835ed9b6da8f4bfd285:home-manager/modules/hyprland/default.nix
