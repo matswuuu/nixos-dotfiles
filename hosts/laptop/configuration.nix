e581c494d99fb3b24862e6e470a7cbcd25cf7146
matswuuu <matswuuu@gmail.com>
2026-06-25 01:04:10 +1000

    .

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -3,6 +3,7 @@
   imports = [
     ../../nixos/profiles/desktop-niri.nix
     ./networking.nix
+    ./game/steam.nix
     ./hardware/auto-cpufreq.nix
     ./hardware/hardware-configuration.nix
   ];
