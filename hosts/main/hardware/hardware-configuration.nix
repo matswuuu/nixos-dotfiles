85ff3a4a69c3d11e6534b3dd5e224def7a96c064
matswuuu <matswuuu@gmail.com>
2026-07-01 05:22:57 +1000

    kernel

--- hosts/main/hardware/hardware-configuration.nix
+++ hosts/main/hardware/hardware-configuration.nix
@@ -7,6 +7,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.supportedFilesystems = [ "ntfs" ];
